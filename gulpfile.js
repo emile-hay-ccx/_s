4980d1b8aef9c4b5ecac452abfb964b621491858
Emile Hay <emileh@commcentrix.com>
2017-01-25 16:39:02 +0200

    Redoing the whole Gulp thing.

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,19 @@
 
 // grab our gulp packages
 var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass');
 
 // create a default task and just log a message
 gulp.task('default', function() {
     return gutil.log('Gulp is running!')
 });
+
+gulp.task('sass', function(){
+    return gulp.src('sass/style.scss')
+        .pipe(sass()) // Using gulp-sass
+        .pipe(gulp.dest('style.css'))
+});
+
+// Gulp watch syntax
+gulp.watch('sass/**/*.scss', ['sass']);
